f2a0a8bab31cc3df9bc73c2cb39ab7b85275d9d8
Racso1624 <fer10_lb@outlook.com>
2023-05-31 01:00:16 -0600

    Se hacen cambios

--- src/backend/controllers/usersController.js
+++ src/backend/controllers/usersController.js
@@ -6,9 +6,7 @@ class UserController {
     console.log(req.body)
     try {
         const { email, password, fullname } = req.body
-        const new_email = new String(email.val)
-        const new_fullname = new String(fullname.val)
-        const result = await session.run(`CREATE (u:User {fullname: ${new_fullname}, email: ${new_email}, password: ${password.val} })`)
+        const result = await session.run(`CREATE (u:User {fullname: '${fullname.val}', email: '${email.val}', password: ${password.val} })`)
         res.send(result.records)
     } catch (error) {
         res.status(500).send(error)
