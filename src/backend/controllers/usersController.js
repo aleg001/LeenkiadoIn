f50562f28c13fb0cad2efa384ccf7725f4f32558
Ale Gomez <gom20347@uvg.edu.gt>
2023-05-31 02:37:42 -0600

    fixed bug

--- src/backend/controllers/usersController.js
+++ src/backend/controllers/usersController.js
@@ -5,13 +5,27 @@ class UserController {
     const session = driver.session()
     console.log(req.body)
     try {
-        const { email, password, fullname } = req.body
-        const result = await session.run(`CREATE (u:User {fullname: '${fullname.val}', email: '${email.val}', password: ${password.val} })`)
-        res.send(result.records)
+      const { email, password, fullname } = req.body
+      const result = await session.run(
+        `CREATE (u:User {fullname: '${fullname.val}', email: '${email.val}', password: '${password.val}' })`
+      )
+      res.send(result.records)
     } catch (error) {
-        res.status(500).send(error)
+      res.status(500).send(error)
     } finally {
-        await session.close()
+      await session.close()
+    }
+  }
+  static getUsers = async (req, res) => {
+    const session = driver.session()
+    try {
+      const result = await session.run('MATCH (u:User) RETURN u')
+      const users = result.records.map((record) => record.get('u').properties)
+      res.send(users)
+    } catch (error) {
+      res.status(500).send(error)
+    } finally {
+      await session.close()
     }
   }
 }
--- src/backend/routes/web.js
+++ src/backend/routes/web.js
@@ -3,5 +3,6 @@ const UserController = require('../controllers/usersController.js')
 
 const router = express.Router()
 router.post('/users', UserController.createUser)
+router.get('/users', UserController.getUsers)
 
 module.exports = router
